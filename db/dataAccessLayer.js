89e853a8bb7d1a85514f9666de8a64ca7bbfdf6a
Laukik <Laukik.Ragji@bhge.com>
2018-12-07 21:11:31 +0530

    Added proper fetch from database functionality

--- db/dataAccessLayer.js
+++ db/dataAccessLayer.js
@@ -4,12 +4,38 @@ class DAL {
     constructor() {
         this.getProductById = this.getProductById.bind(this);
         this.pool = this.pool.bind(this);
-        
-        //let insertQuery = "insert into products (name,price, discount,image,description,keywords,meta) values($1,$2,$3,$4,$5,$6,$7);"
-        // for(i-0;i<100;i++)
-        // {
-        //  pgPool.query(insertQuery, ['ProductName' + i, (i * 1000), i, '{"./static/resources/images/Product' + i + '.jpg","./static/resources/images/Product_sm' + i + '.jpg"}', 'Yet to find', '{"Product' + i + '","anything"}', '{"key":"value"}'])
-        // };
+
+//         let insertQuery = "insert into products (mid,name,price, discount,image,description,ingredients,keywords,meta,sys_meta)  values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);"
+//         for (let i = 0; i < 50000; i++) {
+//             pgPool.query(insertQuery, [1, 'ProductName' + i,
+//                 (i * 1000), i,
+//                 '{"./static/resources/images/Product1.jpg","./static/resources/images/Product2.jpg","./static/resources/images/Product3.jpg","./static/resources/images/Product4.jpg"}',
+//                 '<ul><li>Protection from heart attack and stroke.</li><li>Lowers triglycerides, LDL and increases HDL.</li><li>Helps maintain healthy joints.</li><li>Key component of the brain and eye.</li><li>Important in the growth and development of the foetal brain during pregnancy.</li><li>Improves skin and eye health.</li><li>Helps in psoriasis and eczema.</li><li>Provide lubrication to the skin, arteries, veins and intestinal tract.</li><li>Helps in reducing depression.</li><li>Helps in Attention Deficit/Hyperactivity Disorder (ADHD)</li><li>Helps maintain normal blood sugar levels.</li><li>Lowers blood pressure.</li><li>Helps in Reducing breast, colon and prostate cancer.</li></ul>',
+//                 '<table border="1" cellpadding="0" cellspacing="0" style="width:84.36%;" width="84%"><tbody><tr><td colspan="3" style="width:100.0%;"><p><strong>Supplement Facts:</strong></p></td></tr><tr><td colspan="3" style="width:100.0%;"><p><strong>Serving Size:</strong>&nbsp;1 Capsule</p></td></tr><tr><td>&nbsp;</td><td style="width:21.72%;"><p align="center"><strong>Amount Per Serving</strong></p></td><td style="width:20.22%;"><p align="center"><strong>% DV</strong></p></td></tr><tr><td><p>MegaNatural-BP<br>Grape Seed Extract<br>Vitus Vinifera Seed Standardized to 90% Polyphenols</p></td><td style="width:21.72%;"><p align="center">300 mg</p></td><td style="width:20.22%;"><p align="center">*</p></td></tr><tr><td colspan="3" style="width:100.0%;"><p>*Daily Value (DV) not established.</p></td></tr></tbody></table>',
+//                 '{"Search","Me","Product","Hello","World","Productç' + i + '"}',
+//                 `{
+//     "Category":"Diet",
+// "Sub-Category":"Unknown",
+// "code":"HO7914",
+// "package_detail":"60veggie capsules",
+// "serving_per_container":"This bottle will last 60 days",
+// "serving_size":"1 capsule",
+// "max_quantity":10,
+// "shipping_time":"Ships in 3 days"
+// }`,
+//                 `{
+//     "position":20,
+//     "sale":false,
+//     "isNew":false,
+//     "isTop":false,
+//     "isActive":true,
+//     "amazon":false,
+//     "is_amazon_blocked":false,
+//     "is_liquid":true
+// }`
+//             ]).catch((err) => console.error(err))
+
+//         };
     }
 
     pool() {
@@ -17,8 +43,10 @@ class DAL {
     }
 
     getProductById(productId) {
-        return pgPool.query("select * from products where id=$1",[productId])
-        .then((res)=>{return res.rows[0]});
+        return pgPool.query(`SELECT manufacturer.name as mname,manufacturer.website as website,manufacturer.image as mimage,products.*
+        FROM products JOIN manufacturer ON (products.mid = manufacturer.id) 
+        where products.id=$1`, [productId])
+            .then((res) => { return res.rows[0] });
     }
 }
 
--- db/seedscripts/v1.0/2 producttable.sql
+++ db/seedscripts/v1.0/2 producttable.sql
@@ -1,17 +1,69 @@
+create table manufacturer (
+"id" serial primary key,
+"name" varchar(250) not null,
+"image" text[],
+"website" varchar(250)
+);
+
 create table products (
 "id" serial Primary key,
+"mid" integer REFERENCES manufacturer(id) not null,
 "name" varchar(250) not null,
 "price" float not null,
 "discount" float  not null,
 "image" text[] not null,
-"description" varchar(500),
+"description" varchar(2000),
+"ingredients" varchar(2000),
 "keywords" text[], 
-"meta" jsonb
-)
+"meta" jsonb,
+"sys_meta" jsonb
+);
 
+insert into manufacturer (name,image,website) 
+values(
+'HEALTHY ORIGINS',
+ '{"./static/resources/images/M1.jpg",
+ "./static/resources/images/M2.jpg",
+ "./static/resources/images/M3.jpg",
+ "./static/resources/images/M4.jpg"}',
+ 'http://www.healthyorigins.com/'
+);
 
+	insert into products (mid,name,price, discount,image,description,ingredients,keywords,meta,sys_meta) 
+values
+(1,
+'Complete Product',
+ 100,
+ 1,
+ '{"./static/resources/images/Product1.jpg",
+ "./static/resources/images/Product2.jpg",
+ "./static/resources/images/Product3.jpg",
+ "./static/resources/images/Product4.jpg"}',
+ '<ul><li>Protection from heart attack and stroke.</li><li>Lowers triglycerides, LDL and increases HDL.</li><li>Helps maintain healthy joints.</li><li>Key component of the brain and eye.</li><li>Important in the growth and development of the foetal brain during pregnancy.</li><li>Improves skin and eye health.</li><li>Helps in psoriasis and eczema.</li><li>Provide lubrication to the skin, arteries, veins and intestinal tract.</li><li>Helps in reducing depression.</li><li>Helps in Attention Deficit/Hyperactivity Disorder (ADHD)</li><li>Helps maintain normal blood sugar levels.</li><li>Lowers blood pressure.</li><li>Helps in Reducing breast, colon and prostate cancer.</li></ul>',
+ '<table border="1" cellpadding="0" cellspacing="0" style="width:84.36%;" width="84%"><tbody><tr><td colspan="3" style="width:100.0%;"><p><strong>Supplement Facts:</strong></p></td></tr><tr><td colspan="3" style="width:100.0%;"><p><strong>Serving Size:</strong>&nbsp;1 Capsule</p></td></tr><tr><td>&nbsp;</td><td style="width:21.72%;"><p align="center"><strong>Amount Per Serving</strong></p></td><td style="width:20.22%;"><p align="center"><strong>% DV</strong></p></td></tr><tr><td><p>MegaNatural-BP<br>Grape Seed Extract<br>Vitus Vinifera Seed Standardized to 90% Polyphenols</p></td><td style="width:21.72%;"><p align="center">300 mg</p></td><td style="width:20.22%;"><p align="center">*</p></td></tr><tr><td colspan="3" style="width:100.0%;"><p>*Daily Value (DV) not established.</p></td></tr></tbody></table>',
+ '{"Search","Me","Product","Hello","World"}',
+ '{
+	 "Category":"Diet",
+ "Sub-Category":"Unknown",
+ "code":"HO7914",
+ "package_detail":"60veggie capsules",
+ "serving_per_container":"This bottle will last 60 days",
+ "serving_size":"1 capsule",
+ "max_quantity":10,
+ "shipping_time":"Ships in 3 days"
+ }',
+ '{
+	 "position":20,
+	 "sale":false,
+	 "isNew":false,
+	 "isTop":false,
+	 "isActive":true,
+	 "amazon":false,
+	 "is_amazon_blocked":false,
+	 "is_liquid":true
+ }');
 
-	-- [product_id] [int] IDENTITY(1000,1) NOT NULL,
+ 	-- [product_id] [int] IDENTITY(1000,1) NOT NULL,
 	-- [brand_id] [int] NOT NULL,
 	-- [product_name] [varchar](250) NOT NULL,
 	-- [part_no] [varchar](100) NULL,
