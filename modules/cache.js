0271343595536efc65b8f142f5617ca0dedfda7d
Laukik On Windows Home <22937426+LRagji@users.noreply.github.com>
2018-12-30 11:11:17 +0530

    Add new in memory cache

--- modules/cache.js
+++ modules/cache.js
@@ -1,14 +1,29 @@
-class keyValueCache {
+var cache = require('memory-cache');
+class memCache {
     constructor() {
         this.insert = this.insert.bind(this);
         this.fetch = this.fetch.bind(this);
+        this.update = this.update.bind(this);
     }
 
-    insert() {
-
+    insert(key, value) {
+        cache.put(key, value);
+        console.debug("Current size for cache is " + cache.memsize() + " with " + cache.size + "entries.");
     }
 
-    fetch() {
+    fetch(key) {
+        return cache.get(key);
+    }
 
+    update(key, value) {
+        if (cache.del(key)) {
+            cache.put(key, value);
+            console.debug("Current size for cache is " + cache.memsize() + " with " + cache.size + "entries.");
+        }
+        else {
+            throw new Error("Failed to remove the item from cache.");
+        }
     }
 }
+
+module.exports = memCache;
