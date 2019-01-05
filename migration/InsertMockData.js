31c1d050ff05e69cd1dde873d4248c9e87b02f20
Laukik On Windows Home <22937426+LRagji@users.noreply.github.com>
2019-01-05 14:06:31 +0530

    Added functionality to mock data

---
+++ migration/InsertMockData.js
@@ -0,0 +1,30 @@
+let pg = require('pg')
+let pgPool = new pg.Pool({ user: process.env.DB_USER || 'postgres', host: process.env.DB_HOST || 'localhost', database: process.env.DB_DB || 'Experimental', password: process.env.DB_PASS || 'P@55word', port: 5432, });
+const products = require('../db/products').singleton(pgPool);
+
+async function InsertMockProducts(numberOfProducts) {
+    for (let i = 0; i < numberOfProducts; i++) {
+        await products.createProduct(
+            "Doctor's Best, Best Vitamin C, 1000 mg, 120 Veg " + i,
+            (parseFloat(i) * 100.00 + 1000.00),
+            (parseFloat(i) * 100.00),
+            "default.jpg",
+            '<ul><li>Protection from heart attack and stroke.</li><li>Lowers triglycerides, LDL and increases HDL.</li><li>Helps maintain healthy joints.</li><li>Key component of the brain and eye.</li><li>Important in the growth and development of the foetal brain during pregnancy.</li><li>Improves skin and eye health.</li><li>Helps in psoriasis and eczema.</li><li>Provide lubrication to the skin, arteries, veins and intestinal tract.</li><li>Helps in reducing depression.</li><li>Helps in Attention Deficit/Hyperactivity Disorder (ADHD)</li><li>Helps maintain normal blood sugar levels.</li><li>Lowers blood pressure.</li><li>Helps in Reducing breast, colon and prostate cancer.</li></ul>',
+            '<table border="1" cellpadding="0" cellspacing="0" style="width:84.36%;" width="84%"><tbody><tr><td colspan="3" style="width:100.0%;"><p><strong>Supplement Facts:</strong></p></td></tr><tr><td colspan="3" style="width:100.0%;"><p><strong>Serving Size:</strong>&nbsp;1 Capsule</p></td></tr><tr><td>&nbsp;</td><td style="width:21.72%;"><p align="center"><strong>Amount Per Serving</strong></p></td><td style="width:20.22%;"><p align="center"><strong>% DV</strong></p></td></tr><tr><td><p>MegaNatural-BP<br>Grape Seed Extract<br>Vitus Vinifera Seed Standardized to 90% Polyphenols</p></td><td style="width:21.72%;"><p align="center">300 mg</p></td><td style="width:20.22%;"><p align="center">*</p></td></tr><tr><td colspan="3" style="width:100.0%;"><p>*Daily Value (DV) not established.</p></td></tr></tbody></table>',
+            "C" + i.toString(),
+            "180 Softgels",
+            "1 Softgels",
+            "This bottle will last 180 days.",
+            i + 1,
+            "Category" + i.toString(),
+            "Sub Category" + i.toString(),
+            "NOW FOODS",
+            "https://www.health-mall.in",
+            [],
+            "search Laukik Ragji Hello",
+            undefined
+        );
+    }
+}
+
+InsertMockProducts(50);
