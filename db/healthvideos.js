94f70f8e2519a9f940c983d323e6f6a6e7735726
Laukik On Windows Home <22937426+LRagji@users.noreply.github.com>
2019-02-01 20:58:28 +0530

    Added Ingredients and health topics

--- db/healthvideos.js
+++ db/healthvideos.js
@@ -1,4 +1,6 @@
 let videos = [];
+let ingredients = [];
+let healthConditions = [];
 
 class healthVideos {
 
@@ -8,10 +10,26 @@ class healthVideos {
         this.readHealthVideoById = this.readHealthVideoById.bind(this);
         this.readHealthVideos = this.readHealthVideos.bind(this);
         this.updateHealthVideo = this.updateHealthVideo.bind(this);
+        this.readIngredients = this.readIngredients.bind(this);
+        this.readIngredientById = this.readIngredientById.bind(this);
+        this.readHealthConditions = this.readHealthConditions.bind(this);
+        this.readHealthConditionsById = this.readHealthConditionsById.bind(this);
 
         if (videos.length === 0) {
             for (let i = 0; i < 20; i++) {
                 this.createHealthVideo("Laukik" + i, "Video Text", '<iframe width="560" height="315" src="https://www.youtube.com/embed/nm1lYAvx2mw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>', [], []);
+            }
+        }
+
+        if (ingredients.length === 0) {
+            for (let i = 0; i < 20; i++) {
+                ingredients.push({ name: "Ingredient" + i, id: i });
+            }
+        }
+
+        if (healthConditions.length === 0) {
+            for (let i = 0; i < 20; i++) {
+                healthConditions.push({ name: "Health condition" + i, id: i });
             }
         }
     }
@@ -79,6 +97,60 @@ class healthVideos {
                 else {
                     acc(undefined);
                 }
+            }
+            catch (ex) {
+                rej(ex);
+            }
+        });
+    }
+
+    readIngredients() {
+        return new Promise((acc, rej) => {
+            try {
+                acc(ingredients.map((e) => Object.assign({}, e)));
+            }
+            catch (ex) {
+                rej(ex);
+            }
+        });
+    }
+
+    readIngredientById(id) {
+        return new Promise((acc, rej) => {
+            try {
+                id = parseInt(id);
+                let idx = ingredients.findIndex((l) => l.id === id);
+                if (ingredients < 0)
+                    acc(undefined);
+                else
+                    acc(Object.assign({}, ingredients[idx]));
+            }
+            catch (ex) {
+                rej(ex);
+            }
+        });
+    }
+
+    readHealthConditions() {
+        return new Promise((acc, rej) => {
+            try {
+                acc(healthConditions.map((e) => Object.assign({}, e)));
+            }
+            catch (ex) {
+                rej(ex);
+            }
+        });
+    }
+
+    readHealthConditionsById(id) {
+        return new Promise((acc, rej) => {
+            try {
+                id = parseInt(id);
+                let idx = healthConditions.findIndex((l) => l.id === id);
+                if (healthConditions < 0)
+                    acc(undefined);
+                else
+                    acc(Object.assign({}, healthConditions[idx]));
             }
             catch (ex) {
                 rej(ex);
