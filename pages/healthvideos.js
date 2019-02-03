8ad80b5dfa1dc3415afc4dda29f62c49c9c490c8
Laukik On Windows Home <22937426+LRagji@users.noreply.github.com>
2019-02-03 14:25:00 +0530

    Css framework

--- pages/healthvideos.js
+++ pages/healthvideos.js
@@ -14,9 +14,28 @@ class pageHealthVideos extends page {
     }
 
     async renderVideos(req, renderView) {
+        let healthVideos = await this.dal.healthVideos.readHealthVideos();
+        let healthConditions = [], ingredients = [], foundIngredientsId = [], foundHealthConditionId = [];
+        healthVideos.forEach(video => {
+            video.healthConditions.forEach((healthCondition) => {
+                if (foundHealthConditionId.indexOf(healthCondition) < 0) {
+                    healthConditions.push(this.dal.healthVideos.readHealthConditionsById(healthCondition));
+                    foundHealthConditionId.push(healthCondition);
+                }
+            });
+            video.ingredients.forEach((ingredient) => {
+                if (foundIngredientsId.indexOf(ingredient) < 0) {
+                    ingredients.push(this.dal.healthVideos.readIngredientById(ingredient));
+                    foundIngredientsId.push(ingredient);
+                }
+            });
+        });
+
+        ingredients = await Promise.all(ingredients);
+        healthConditions = await Promise.all(healthConditions);
         let pageData = {};
-        pageData[this.const.healthConditions] = this.util.sortArrayByProperty(await this.dal.healthVideos.readHealthConditions(), "name");
-        pageData[this.const.ingredients] = this.util.sortArrayByProperty(await this.dal.healthVideos.readIngredients(), "name");
+        pageData[this.const.healthConditions] = this.util.sortArrayByProperty(await healthConditions, "name");
+        pageData[this.const.ingredients] = this.util.sortArrayByProperty(await ingredients, "name");
         pageData[this.const.healthVideos] = this.util.sortArrayByProperty(await this.dal.healthVideos.readHealthVideos(), "name");
         renderView('../pages/healthvideos', pageData);
     }
