622c875656639b5568dc50e3a924eccb247e1b55
Laukik On Windows Home <22937426+LRagji@users.noreply.github.com>
2019-02-17 16:51:03 +0530

    Fixed dist folder

--- bundle.bat
+++ bundle.bat
@@ -1,8 +1,14 @@
-copy "package.json" "dist\package.json" 
-copy "package-lock.json" "dist\package-lock.json"
-copy "index.js" "dist\index.js"
-echo F| Xcopy "db\dataAccessLayer.js" "dist\db\dataAccessLayer.js"
+echo F| Xcopy  "package.json" "dist\package.json" 
+echo F| Xcopy "package-lock.json" "dist\package-lock.json"
+echo F| Xcopy "index.js" "dist\index.js"
+echo F| Xcopy "ecosystem.config.js" "dist\ecosystem.config.js"
+Xcopy /I /S /E "db" "dist\db"
 Xcopy /I /S /E "static" "dist\static"
 Xcopy /I /S /E "pages" "dist\pages"
 Xcopy /I /S /E "modules" "dist\modules"
-ECHO "Done"
+
+REM Delete all files except Default.jpg
+cd "\dist\static\resources\images\products"
+for %i in (*) do if not "%~i" == "deafult.jpg" del "%~i"
+
+ECHO "Done Please zip the dist folder."
